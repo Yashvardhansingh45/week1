12c1f1195b1ba51a7cb8a4ddd910269abdb9367e
Yash vardhan Singh <yashvardhansinghparihar10@gmail.com>
2025-01-14 16:07:41 +0530

    LEVEL-1

---
+++ Day2/Level1/CanVote.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class CanVote {
+    public static void main(String[] args) {
+        // Create a Scanner object to take user input
+        Scanner input = new Scanner(System.in);
+        // Get age input from the user
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        // Check if age is greater or equal to 18
+        if (age >= 18) {
+            System.out.println("The person's age is " + age + " and can vote.");
+        } else {
+            System.out.println("The person's age is " + age + " and cannot vote.");
+        }
+        input.close();
+    }
+}
